b19d1c86a49bad2744cdc155f975cabc6c2e95f4
julianabety <julianabety@hotmail.com>
2026-02-03 14:56:55 -0300

    feat: implementa componente Header com navegação e menu de usuário

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,44 @@
 import { Link } from 'react-router-dom';    
 import Logo from '../../assets/images/logo.png';
 import S from './header.module.scss';
+import { useState } from 'react';
 
 export default function Header() {
-  return (
 
+ const [menuOpen, setMenuOpen] = useState(false);
+
+ 
+  return (
     <>
     
     <header className={S.header}>
       <section>
 
         <Link to="/">
-          <img  src={Logo} alt="Logo"/>
+          <img className={S.imgLogo}  src={Logo} alt="Logotipo"/>
         </Link>
 
       </section>
 
-      <nav>
-          <ul>
-            <Link to="/doacao">Doação</Link>
-            <Link to="/voluntariado">Voluntariado</Link>
-            <Link to="/mentoria">Mentoria</Link>
-            <Link to="/eventos">Eventos</Link>
-          </ul>
-        </nav>
+      <nav className={S.nav}>
+        <Link to="/doacao">Doação</Link>
+        <Link to="/voluntariado">Voluntariado</Link>
+        <Link to="/mentoria">Mentoria</Link>
+        <Link to="/eventos">Eventos</Link>
+      </nav>
       
-      <img src="https://avatars.githubusercontent.com/u/187912049?v=4" alt=""/>
+      <img className={S.imgUser} src="https://avatars.githubusercontent.com/u/187912049?v=4" alt="Imagem do usuário" 
+      onClick={() => setMenuOpen (!menuOpen)}/>
     </header>
 
-    <Link to="/usuario">Juliana Ferreira</Link>
+      <nav className={menuOpen ? S.navUser : S.closedNav} >
+        <Link to="/usuario">Juliana Ferreira</Link>
+        <Link>Meu Voluntariado</Link>
+        <Link>Configurações de conta</Link>
+        <Link>Sair</Link>
+      </nav>
+
+    
     </>
   )
 }
